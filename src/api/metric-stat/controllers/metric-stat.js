cec9b07dfe32a78c6efea679fd2e46ca8a26f679
Sandeep Dravid <sandeep.dravid@ansys.com>
2023-06-14 12:17:12 +0530

    first commit

---
+++ src/api/metric-stat/controllers/metric-stat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * metric-stat controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::metric-stat.metric-stat');
---
+++ src/api/metric-stat/routes/metric-stat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * metric-stat router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::metric-stat.metric-stat');
